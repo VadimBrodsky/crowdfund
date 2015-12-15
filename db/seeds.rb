bdcd668318d0e88294a1c22984b84d88db8f04fd
Vadim Brodsky <vadimb@crankworks.ca>
2015-12-15 09:33:32 -0500

    Add seeds and connect to view

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Project.create(name: 'Music Video', description: 'Super aweome music video', target_pledge_amount: 30000.00)
-Project.create(name: 'Hacker Keyboard', description: 'Mechanical switches for the hackers', target_pledge_amount: 12000.00)
-Project.create(name: 'Code Editor', description: 'Interactive REPL editor with battaries', target_pledge_amount: 20000.00)
-Project.create(name: 'JavaScript MVC Framework', description: 'The one framework to rule them all', target_pledge_amount: 90000.00)
+Project.create(name: 'Music Video', description: 'Super aweome music video', target_pledge_amount: 30000.00, website: 'http://www.music.video', pledging_ends_on: 20.days.from_now)
+Project.create(name: 'Ultimate Hacking Keyboard', description: 'Mechanical switches for the hackers', target_pledge_amount: 200_000.00, website: 'https://www.crowdsupply.com/ugl/ultimate-hacking-keyboard', pledging_ends_on: 20.days.from_now)
+Project.create(name: 'Code Editor', description: 'Interactive REPL editor with battaries', target_pledge_amount: 20000.00, website: 'http://lighttable.com/', pledging_ends_on: 10.days.from_now)
+Project.create(name: 'JavaScript MVC Framework', description: 'The one framework to rule them all', target_pledge_amount: 90000.00, website: 'http://todomvc.com/', pledging_ends_on: 900.days.from_now)
