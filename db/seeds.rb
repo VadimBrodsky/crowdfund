92ddb5499a736c91bce08265d6743e169cadf458
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-12-15 21:59:27 -0500

    Add seeds to include an expired event

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,4 @@ Project.create(name: 'Music Video', description: 'Super aweome music video', tar
 Project.create(name: 'Ultimate Hacking Keyboard', description: 'Mechanical switches for the hackers', target_pledge_amount: 200_000.00, website: 'https://www.crowdsupply.com/ugl/ultimate-hacking-keyboard', pledging_ends_on: 20.days.from_now)
 Project.create(name: 'Code Editor', description: 'Interactive REPL editor with battaries', target_pledge_amount: 20000.00, website: 'http://lighttable.com/', pledging_ends_on: 10.days.from_now)
 Project.create(name: 'JavaScript MVC Framework', description: 'The one framework to rule them all', target_pledge_amount: 90000.00, website: 'http://todomvc.com/', pledging_ends_on: 900.days.from_now)
+Project.create(name: 'iPod Charging Dock', description: 'A high quiality iPod charging dock made from aircraft aluminum and titanium, be ready for anything!', target_pledge_amount: 100000.00, website: 'http://www.adock.com', pledging_ends_on: 3.days.ago)
