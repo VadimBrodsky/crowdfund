c12803d7ed23e5d47760f27f21aaab9004bfc58d
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-12-19 16:30:55 -0500

    Add the rest of the specs

--- spec/features/list_projects_spec.rb
+++ spec/features/list_projects_spec.rb
@@ -11,28 +11,50 @@ describe 'Viewing the list of projects' do
     target_pledge_amount: 200_000.00,
     website: 'https://www.crowdsupply.com/ugl/ultimate-hacking-keyboard',
     pledging_ends_on: 20.days.from_now)
-  project3 = Project.create(name: 'Code Editor',
-    description: 'Interactive REPL editor with battaries',
-    target_pledge_amount: 20000.00,
-    website: 'http://lighttable.com/',
-    pledging_ends_on: 10.days.from_now)
-  project4 = Project.create(name: 'JavaScript MVC Framework',
-    description: 'The one framework to rule them all',
-    target_pledge_amount: 90000.00,
-    website: 'http://todomvc.com/',
-    pledging_ends_on: 900.days.from_now)
-  project5 = Project.create(name: 'iPod Charging Dock',
+  project3 = Project.create(name: 'iPod Charging Dock',
     description: 'A high quiality iPod charging dock made from aircraft aluminum and titanium, be ready for anything!',
     target_pledge_amount: 100000.00,
     website: 'http://www.adock.com',
     pledging_ends_on: 3.days.ago)
 
-  it 'shows the project namess' do
+
+  it 'shows the names' do
     visit projects_url
+
     expect(page).to have_text(project1.name)
     expect(page).to have_text(project2.name)
     expect(page).to have_text(project3.name)
-    expect(page).to have_text(project4.name)
-    expect(page).to have_text(project5.name)
+  end
+
+  it 'shows the description truncated' do
+    visit projects_url
+
+    expect(page).to have_text(project1.description[0..10])
+    expect(page).to have_text(project2.description[0..10])
+    expect(page).to have_text(project3.description[0..10])
+  end
+
+  it 'shows the target pledge amount in dollars' do
+    visit projects_url
+
+    expect(page).to have_text('$30,000.00')
+    expect(page).to have_text('$200,000.00')
+    expect(page).to have_text('$100,000.00')
+  end
+
+  it 'shows the website adderss' do
+    visit projects_url
+
+    expect(page).to have_text(project1.website)
+    expect(page).to have_text(project2.website)
+    expect(page).to have_text(project3.website)
+  end
+
+  it 'shows the end date for pledging' do
+    visit projects_url
+
+    expect(page).to have_text(project1.pledging_ends_on)
+    expect(page).to have_text(project2.pledging_ends_on)
+    expect(page).to have_text(project3.pledging_ends_on)
   end
 end
