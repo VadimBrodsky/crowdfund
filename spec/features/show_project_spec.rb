57550dbe792741f520a08d8f55714ce0ccdf5567
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-12-22 07:40:58 -0500

    Add spec for to show individual project

---
+++ spec/features/show_project_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'Viewing an individual project' do
+  it 'shows the project details' do
+    project = Project.create(name: "Start-Up Project",
+                            description: "A description of a start-up project",
+                            target_pledge_amount: 100.00,
+                            pledging_ends_on: 1.day.from_now,
+                            website: "http://project-a.com")
+
+    visit project_url(project)
+
+    expect(page).to have_text(project.name)
+    expect(page).to have_text(project.description)
+    expect(page).to have_text('$100.00')
+    expect(page).to have_text(project.website)
+    expect(page).to have_text(project.pledging_ends_on)
+  end
+end
