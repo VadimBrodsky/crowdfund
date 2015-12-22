34c25c9db4fdd97c50c814b3ba7a2d17cd0b4968
Vadim Brodsky <vadimb@crankworks.ca>
2015-12-22 14:06:21 -0500

    Cleaned up the specs

--- spec/features/list_projects_spec.rb
+++ spec/features/list_projects_spec.rb
@@ -1,35 +1,35 @@
-require 'rails_helper'
-
 describe 'Viewing the list of projects' do
-
   before(:all) do
-    @project1 = Project.create(name: 'Music Video',
-        description: 'Super aweome music video',
-        target_pledge_amount: 30000.00,
-        website: 'http://www.music.video',
-        pledging_ends_on: 20.days.from_now)
-    @project2 = Project.create(name: 'Ultimate Hacking Keyboard',
-        description: 'Mechanical switches for the hackers',
-        target_pledge_amount: 200_000.00,
-        website: 'https://www.crowdsupply.com/ugl/ultimate-hacking-keyboard',
-        pledging_ends_on: 20.days.from_now)
-    @project3 = Project.create(name: 'iPod Charging Dock',
-        description: 'A high quiality iPod charging dock made from aircraft aluminum and titanium, be ready for anything!',
-        target_pledge_amount: 100000.00,
-        website: 'http://www.adock.com',
-        pledging_ends_on: 3.days.ago)
-
+    @project1 = Project.create(
+      name: 'Music Video',
+      description: 'Super aweome music video',
+      target_pledge_amount: 30_000.00,
+      website: 'http://www.music.video',
+      pledging_ends_on: 20.days.from_now)
+    @project2 = Project.create(
+      name: 'Ultimate Hacking Keyboard',
+      description: 'Mechanical switches for the hackers',
+      target_pledge_amount: 200_000.00,
+      website: 'https://www.crowdsupply.com/ugl/ultimate-hacking-keyboard',
+      pledging_ends_on: 20.days.from_now)
+    @project3 = Project.create(
+      name: 'iPod Charging Dock',
+      description: 'A high quiality iPod charging dock made from aircraft
+                    aluminum and titanium, be ready for anything!',
+      target_pledge_amount: 100_000.00,
+      website: 'http://www.adock.com',
+      pledging_ends_on: 3.days.ago)
   end
 
   after(:all) do
-    [@project1, @project2, @project3].each { |p| p.destroy }
+    [@project1, @project2, @project3].each(&:destroy)
   end
 
   it 'shows the number of projects' do
     visit projects_url
 
     # expect(page).to have_text("#{Project.count} Projects")
-    expect(page).to have_text("3 Projects")
+    expect(page).to have_text('3 Projects')
   end
 
   it 'shows the names' do
--- spec/features/show_project_spec.rb
+++ spec/features/show_project_spec.rb
@@ -1,12 +1,11 @@
-require 'rails_helper'
-
 describe 'Viewing an individual project' do
   before(:all) do
-    @project = Project.create(name: "Start-Up Project",
-                            description: "A description of a start-up project",
-                            target_pledge_amount: 100.00,
-                            pledging_ends_on: 1.day.from_now,
-                            website: "http://project-a.com")
+    @project = Project.create(
+      name: 'Start-Up Project',
+      description: 'A description of a start-up project',
+      target_pledge_amount: 100.00,
+      pledging_ends_on: 1.day.from_now,
+      website: 'http://project-a.com')
   end
 
   after(:all) do
