43fae9259d5a85e9a69308acda08c74053ff57de
Vadim Brodsky <vadimb@crankworks.ca>
2015-12-22 14:48:12 -0500

    Add navigation feature specs

--- spec/features/navigate_spec.rb
+++ spec/features/navigate_spec.rb
@@ -1,5 +1,23 @@
 describe 'Navigating projects' do
-  it 'allows navigation from the detial page to the listing page'
+  before(:all) do
+    @project = Project.create(project_attributes)
+  end
 
-  it 'allows navigation from the listing page to the detial page'
+  after(:all) do
+    @project.destroy
+  end
+
+  it 'allows navigation from the detail page to the listing page' do
+    visit project_url(@project)
+    click_link 'All Projects'
+
+    expect(current_url).to eq(projects_url)
+  end
+
+  it 'allows navigation from the listing page to the detial page' do
+    visit projects_url
+    click_link @project.name
+
+    expect(current_url).to eq(project_url(@project))
+  end
 end
