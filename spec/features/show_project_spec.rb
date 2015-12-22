f6c383c8cc918ee33b2931753e99ffb75e4aa59c
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-12-22 07:54:33 -0500

    Refactor the spec with separate expectations

--- spec/features/show_project_spec.rb
+++ spec/features/show_project_spec.rb
@@ -1,19 +1,45 @@
 require 'rails_helper'
 
 describe 'Viewing an individual project' do
-  it 'shows the project details' do
-    project = Project.create(name: "Start-Up Project",
+  before(:all) do
+    @project = Project.create(name: "Start-Up Project",
                             description: "A description of a start-up project",
                             target_pledge_amount: 100.00,
                             pledging_ends_on: 1.day.from_now,
                             website: "http://project-a.com")
+  end
 
-    visit project_url(project)
+  after(:all) do
+    @project.destroy
+  end
 
-    expect(page).to have_text(project.name)
-    expect(page).to have_text(project.description)
+  it 'shows the project name' do
+    visit project_url(@project)
+
+    expect(page).to have_text(@project.name)
+  end
+
+  it 'shows the project description' do
+    visit project_url(@project)
+
+    expect(page).to have_text(@project.description)
+  end
+
+  it 'shows the target pledge amount in dollard' do
+    visit project_url(@project)
+
     expect(page).to have_text('$100.00')
-    expect(page).to have_text(project.website)
-    expect(page).to have_text(project.pledging_ends_on)
+  end
+
+  it 'shows the project website' do
+    visit project_url(@project)
+
+    expect(page).to have_text(@project.website)
+  end
+
+  it 'shows the project end pledge date' do
+    visit project_url(@project)
+
+    expect(page).to have_text(@project.pledging_ends_on)
   end
 end
