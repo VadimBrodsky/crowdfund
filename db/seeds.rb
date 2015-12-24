ebf38e6fb4f0248614a8eac169b4ea5a3dd5b7de
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-12-23 23:31:32 -0600

    Improve formatting on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,40 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Project.create(name: 'Music Video', description: 'Super aweome music video', target_pledge_amount: 30000.00, website: 'http://www.music.video', pledging_ends_on: 20.days.from_now)
-Project.create(name: 'Ultimate Hacking Keyboard', description: 'Mechanical switches for the hackers', target_pledge_amount: 200_000.00, website: 'https://www.crowdsupply.com/ugl/ultimate-hacking-keyboard', pledging_ends_on: 20.days.from_now)
-Project.create(name: 'Code Editor', description: 'Interactive REPL editor with battaries', target_pledge_amount: 20000.00, website: 'http://lighttable.com/', pledging_ends_on: 10.days.from_now)
-Project.create(name: 'JavaScript MVC Framework', description: 'The one framework to rule them all', target_pledge_amount: 90000.00, website: 'http://todomvc.com/', pledging_ends_on: 900.days.from_now)
-Project.create(name: 'iPod Charging Dock', description: 'A high quiality iPod charging dock made from aircraft aluminum and titanium, be ready for anything!', target_pledge_amount: 100000.00, website: 'http://www.adock.com', pledging_ends_on: 3.days.ago)
+Project.create!([
+  {
+    name: 'Music Video',
+    description: 'Super aweome music video',
+    target_pledge_amount: 30000.00,
+    website: 'http://www.music.video',
+    pledging_ends_on: 20.days.from_now
+  },
+  {
+    name: 'Ultimate Hacking Keyboard',
+    description: 'Mechanical switches for the hackers',
+    target_pledge_amount: 200_000.00,
+    website: 'https://www.crowdsupply.com/ugl/ultimate-hacking-keyboard',
+    pledging_ends_on: 20.days.from_now
+  },
+  {
+    name: 'Code Editor',
+    description: 'Interactive REPL editor with battaries',
+    target_pledge_amount: 20000.00,
+    website: 'http://lighttable.com/',
+    pledging_ends_on: 10.days.from_now
+  },
+  {
+    name: 'JavaScript MVC Framework',
+    description: 'The one framework to rule them all',
+    target_pledge_amount: 90000.00,
+    website: 'http://todomvc.com/',
+    pledging_ends_on: 900.days.from_now
+  },
+  {
+    name: 'iPod Charging Dock',
+    description: 'A high quiality iPod charging dock made from aircraft aluminum and titanium, be ready for anything!',
+    target_pledge_amount: 100000.00,
+    website: 'http://www.adock.com',
+    pledging_ends_on: 3.days.ago
+  }
+])
