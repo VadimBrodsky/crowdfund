9309fc9513a712394eaae1a9bc5fae94daaaf49c
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-12-28 17:18:09 -0500

    Convert named routes to a resource

--- config/routes.rb
+++ config/routes.rb
@@ -53,9 +53,13 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
   root 'projects#index'
-  get 'projects' => 'projects#index'
-  get 'projects/:id' => 'projects#show', as: 'project'
-  get 'projects/:id/edit' => 'projects#edit', as: 'edit_project'
-  patch 'projects/:id' => 'projects#update'
+
+  # get 'projects' => 'projects#index'
+  # get 'projects/:id' => 'projects#show', as: 'project'
+  # get 'projects/:id/edit' => 'projects#edit', as: 'edit_project'
+  # patch 'projects/:id' => 'projects#update'
+  resources :projects
+
 end
