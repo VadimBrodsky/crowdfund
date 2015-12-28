87156d9a141e966ed6b82d2ed601a0ff8b0e9768
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-12-28 17:12:08 -0500

    Finish spec

--- spec/features/create_project_spec.rb
+++ spec/features/create_project_spec.rb
@@ -12,8 +12,19 @@ describe 'Createing a project' do
   end
 
   it 'creates the project and shows it' do
+    project = Project.create(project_attributes)
     visit new_project_url
 
-    fill_in 'Name'
+    fill_in 'Name', with: project.name
+    fill_in 'Description', with: project.description
+    fill_in 'Target pledge amount', with: project.target_pledge_amount
+    fill_in 'Website', with: poject.website
+
+    click_button 'Create Project'
+
+    expect(current_path).to eq(project_path)
+    expect(page).to have_text(project.name)
+    expect(page).to have_text(project.description)
+    expect(page).to have_text(project.target_pledge_amount)
   end
 end
