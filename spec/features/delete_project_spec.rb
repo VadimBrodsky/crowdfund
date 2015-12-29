1acaa513ffe216c857cc19ffa706a02aeeadf2e5
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-12-29 13:25:18 -0500

    Update spec with the right label

--- spec/features/delete_project_spec.rb
+++ spec/features/delete_project_spec.rb
@@ -6,14 +6,14 @@ describe 'Deleting a project' do
   it 'shows the destroy button on the destory page' do
     visit project_path(@project)
 
-    expect(page).to have_text('Delete')
-    click_link('Delete')
+    expect(page).to have_text('Delete Project')
+    click_link('Delete Project')
   end
 
   it 'destroys the project and shows the project listing without the project' do
     visit project_path(@project)
 
-    click_link('Delete')
+    click_link('Delete Project')
 
     expect(current_url).to eq(projects_url)
     expect(page).not_to have_text(@project.name)
