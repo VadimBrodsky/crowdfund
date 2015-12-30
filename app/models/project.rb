62dd5a5569e72644bf201881f97705e74dfe495f
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-12-29 22:31:46 -0500

    Add custom query for the active projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,8 @@ class Project < ActiveRecord::Base
   def ended?
     pledging_ends_on.blank? || pledging_ends_on < Time.now
   end
+
+  def self.active
+    where('pledging_ends_on >= ?', Time.now).order(pledging_ends_on: :asc)
+  end
 end
