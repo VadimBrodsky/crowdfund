0d709a630c191a945f7929e11a84b7024b87b146
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-01-06 22:59:07 -0500

    Add model validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,19 @@
 class Project < ActiveRecord::Base
+
+  validates :name, :description, presence: true
+  validates :description, length: { maximum: 500 }
+  validates :target_pledge_amount, numericality: { greater_than: 0 }
+  validates :website, allow_blank: true, format:
+    {
+      with: /htt(p|ps)(:\/\/(?:www\.|(?!www))[^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})/i,
+      message: 'must be a valid'
+    }
+  validates :image_file_name, allow_blank: true, format:
+    {
+      with: /\w+\.(gif|jpg|png)\z/i,
+      message: 'must reference a GIF, JPG, or PNG image'
+    }
+
   def ended?
     pledging_ends_on.blank? || pledging_ends_on < Time.now
   end
