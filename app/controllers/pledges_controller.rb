0ed41e28e632b426a946f1a1574175dd1a0db45c
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-01-09 12:03:29 -0500

    Passes happy path for creating a pledge

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -8,4 +8,20 @@ class PledgesController < ApplicationController
     @project = Project.find(params[:project_id])
     @pledge = @project.pledges.new
   end
+
+  def create
+    @project = Project.find(params[:project_id])
+    @pledge = @project.pledges.new(pledge_params)
+    if @pledge.save
+      redirect_to project_pledges_path(@project), notice: 'Thanks for Pledging!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def pledge_params
+    params.require(:pledge).permit(:name, :email, :amount, :comment)
+  end
 end
