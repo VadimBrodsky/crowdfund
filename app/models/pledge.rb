31e35e1526e37708fbc74006c6a04d0b65085149
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-01-09 12:18:08 -0500

    Add message to pledge amount validation

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -2,7 +2,7 @@ class Pledge < ActiveRecord::Base
   AMOUNT_LEVELS = [25.00, 50.00, 100.00, 200.00, 500.00]
 
   validates :name, :email, presence: true
-  validates :amount, inclusion: { in: AMOUNT_LEVELS }
+  validates :amount, inclusion: { in: AMOUNT_LEVELS, message: 'was not picked' }
   validates :email, format:
     {
       with: /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i
