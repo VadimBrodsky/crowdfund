f90e4288a9bdc2d9b6e2c0a18a82e61d788e51b5
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-01-10 16:41:28 -0500

    Add a helper to format the pledge link depending on the funding status

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -14,4 +14,16 @@ module ProjectsHelper
       image_tag(project.image_file_name)
     end
   end
+
+  def format_pledge_link(project)
+    if project.funded?
+      content_tag :h2, 'Funded'
+    else
+      link_to new_project_pledge_path(project), class: 'button ok' do
+        tag1 = content_tag :h3, 'Pledge!', class: 'title'
+        tag2 = content_tag :span, "Only #{number_to_currency(project.amount_outstanding)} left!", class: 'subtitle'
+        (tag1 + tag2).html_safe
+      end
+    end
+  end
 end
